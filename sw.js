ec2bf5637b80eba7de859714124865bc4ef0f528
ggponti74 <ggponti74@hotmail.com>
2026-08-13 15:11:39 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,34 @@
-const CACHE = "v1";
+const CACHE = "v1.0.1";
+
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./index.html"])));
+    e.waitUntil(
+        caches.open(CACHE).then(c =>
+            c.addAll([
+                "./index.html",
+                "./style.css",
+                "./script.js",
+               "./manifest.json"
+           ])
+        )
+    );
 });
+
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+    e.respondWith(
+        caches.match(e.request).then(
+            r => r || fetch(e.request)
+        )
+    );
 });
