74cc3a2dd6bed6ab84f46f92143534a001d4c581
ggponti74 <ggponti74@hotmail.com>
2026-08-18 10:21:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    Stable touch version
    ========================================================= */
 
-const CACHE = "1.1.1";
+const CACHE = "1.1.2";
 
 const ROWS = 14;
 const COLS = 8;
@@ -256,6 +256,8 @@ function createCellElement(row, col) {
   return element;
 };
 
+/*
+
 document.addEventListener("contextmenu", (event) => {
 
   event.preventDefault();
@@ -268,6 +270,8 @@ document.addEventListener("contextmenu", (event) => {
   cycleMark(row, col, element);
 
 });
+
+*/
 
 document.addEventListener(
   "click",
