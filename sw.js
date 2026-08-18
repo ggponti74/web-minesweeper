da4742551d6bb5d9a7ba7fb7fc7d2206850f56d2
ggponti74 <ggponti74@hotmail.com>
2026-08-18 15:43:46 -0400

    Add app.js to service worker cache

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
+importScripts("app.js");
+
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open(CACHE).then(c =>
             c.addAll([
                 "./index.html",
                 "./style.css",
-                "./script.js",
+                "./app.js",
                 "./manifest.json",
                 "./icon-192.png",
                 "./icon-512.png"
