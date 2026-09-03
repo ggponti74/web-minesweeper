88ddbbc89bbf5ef0ca2f23fbbac782ed0a937d63
ggponti74 <ggponti74@hotmail.com>
2026-09-03 19:40:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    Stable touch version
    ========================================================= */
 
-const CACHE = "1.1.__BUILD_VERSION__.2";
+const CACHE = "1.1.__BUILD_VERSION__.4";
 
 const ROWS = 14;
 const COLS = 8;
