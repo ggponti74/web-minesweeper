3e17bf4d88b3f6cdc04320a1eef8af4ab7230d39
ggponti74 <ggponti74@hotmail.com>
2026-09-05 14:35:46 -0400

    Update CACHE version to 1.1.__BUILD_VERSION__I

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    Stable touch version
    ========================================================= */
 
-const CACHE = "1.1.__BUILD_VERSION__H";
+const CACHE = "1.1.__BUILD_VERSION__I";
 const WHATS_NEW = "Added support for high score.";
 
 const ROWS = 14;
