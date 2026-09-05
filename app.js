ae5c4bc8ed4424add1058b8349f5b6fbe1e52340
ggponti74 <ggponti74@hotmail.com>
2026-09-05 13:05:57 -0400

    Update CACHE version from A to B

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    Stable touch version
    ========================================================= */
 
-const CACHE = "1.1.__BUILD_VERSION__A";
+const CACHE = "1.1.__BUILD_VERSION__B";
 
 const ROWS = 14;
 const COLS = 8;
