e0b44a4fe817d66461cef1072d6d6069fd849360
ggponti74 <ggponti74@hotmail.com>
2026-09-14 15:09:38 +0000

    Update CACHE version to 1.2.__BUILD_VERSION__F and fix CSS selector formatting

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    Stable touch version
    ========================================================= */
 
-const CACHE = "1.2.__BUILD_VERSION__E";
+const CACHE = "1.2.__BUILD_VERSION__F";
 
 const ROWS = 16;
 const COLS = 10;
