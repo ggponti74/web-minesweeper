8f37a493d63143f1af26397a39c43862002ac47e
ggponti74 <ggponti74@hotmail.com>
2026-09-15 17:35:36 +0000

    Update CACHE version to 1.2.__BUILD_VERSION__A and add styling for result overlay

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    Stable touch version
    ========================================================= */
 
-const CACHE = "1.2.__BUILD_VERSION__I";
+const CACHE = "1.2.__BUILD_VERSION__A";
 
 const ROWS = 16;
 const COLS = 10;
