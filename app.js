bd5475350c6cdee3e316b5dffdb80252c397c8e2
ggponti74 <ggponti74@hotmail.com>
2026-09-16 11:42:30 -0400

    Update CACHE version to 1.3.__BUILD_VERSION__A

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    Stable touch version
    ========================================================= */
 
-const CACHE = "1.2.__BUILD_VERSION__F";
+const CACHE = "1.3.__BUILD_VERSION__A";
 const LOCAL_STORAGE_VERSION = "minesweeper-state-v1";
 
 const ROWS = 16;
